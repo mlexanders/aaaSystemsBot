3ae4213b14ed08d70a235e42b12ebdb7706975a8
mlexanders <mlexanders@outlook.com>
2023-01-04 22:14:47 +0300

    test endpoint removed

--- aaaSystemsApi/Controllers/ChatController.cs
+++ aaaSystemsApi/Controllers/ChatController.cs
@@ -10,29 +10,6 @@ namespace aaaSystemsApi.Controllers
     {
         public ChatController(BaseCrudRepository<Chat> repository) : base(repository) { }
 
-        [HttpPost("Test")]
-        public async Task Test()
-        {
-            var chat = new Chat()
-            {
-                ChatId = 1,
-                AdminId = 1,
-                UserId = 1,
-                Message = new List<Message>()
-                {
-                    new Message()
-                    {
-                        MessageId = 1
-                    },
-                    new Message()
-                    {
-                        MessageId = 2
-                    }
-                }
-            };
-            await repository.Create(chat);
-        }
-
         public override async Task<List<Chat>> Get()
         {
             return await repository.Read(c => c.Message != null, m => m.Message);
