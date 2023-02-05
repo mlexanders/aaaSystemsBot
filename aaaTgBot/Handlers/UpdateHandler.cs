55106f06d9153742e9596e0d7e946f97d32a15df
mlexanders <mlexanders@outlook.com>
2023-02-06 00:11:07 +0300

    fix delay

--- aaaTgBot/Handlers/UpdateHandler.cs
+++ aaaTgBot/Handlers/UpdateHandler.cs
@@ -9,21 +9,34 @@ namespace aaaTgBot.Handlers
     {
         public static Dictionary<long, ISpecialHandler> BusyUsersIdAndService { get; set; } = new();
 
-        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        public static Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
             LogService.LogUpdate(update);
 
-            if (update.Message is not null)
+            _ = Task.Run(() => Handle(update));
+            return Task.CompletedTask;
+        }
+
+        private static async Task Handle(Update update)
+        {
+            try
             {
-                var chatId = update.Message.Chat.Id;
-                if (!BusyUsersIdAndService.ContainsKey(chatId)) await MainHandler.MessageProcessing(chatId, update.Message);
-                if (BusyUsersIdAndService.ContainsKey(chatId)) BusyUsersIdAndService[chatId].ProcessMessage(update.Message);
+                if (update.Message is not null)
+                {
+                    var chatId = update.Message.Chat.Id;
+                    if (!BusyUsersIdAndService.ContainsKey(chatId)) await MainHandler.MessageProcessing(chatId, update.Message);
+                    if (BusyUsersIdAndService.ContainsKey(chatId)) await BusyUsersIdAndService[chatId].ProcessMessage(update.Message);
+                }
+                else if (update.CallbackQuery is not null)
+                {
+                    var chatId = update.CallbackQuery.Message != null ? update.CallbackQuery.Message.Chat.Id : throw new NotImplementedException();
+                    if (!BusyUsersIdAndService.ContainsKey(chatId)) await MainHandler.CallbackQueryProcessing(chatId, update.CallbackQuery);
+                    if (BusyUsersIdAndService.ContainsKey(chatId)) await BusyUsersIdAndService[chatId].ProcessMessage(update.CallbackQuery.Message);
+                };
             }
-            else if (update.CallbackQuery is not null)
+            catch (Exception e)
             {
-                var chatId = update.CallbackQuery.Message != null ? update.CallbackQuery.Message.Chat.Id : throw new NotImplementedException();
-                if (!BusyUsersIdAndService.ContainsKey(chatId)) await MainHandler.CallbackQueryProcessing(chatId, update.CallbackQuery);
-                if (BusyUsersIdAndService.ContainsKey(chatId)) BusyUsersIdAndService[chatId].ProcessMessage(update.CallbackQuery.Message);
+                LogService.LogError(e.Message);
             }
         }
     }
